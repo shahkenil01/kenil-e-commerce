f4b3b4d71a0091c33728ad14889c922627e3b7fc
shahkenil01 <shahkenil430@gmail.com>
2025-03-29 15:22:30 +0530

    Quick update

--- server/models/products.js
+++ server/models/products.js
@@ -3,44 +3,49 @@ const mongoose = require("mongoose");
 const productSchema = mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: true
   },
   description: {
     type: String,
-    required: true,
+    required: true
   },
+  Images: [{
+    type:String,
+    required: true
+  }
+],
   brand: {
     type: String,
-    default: '',
+    default: ''
   },
   price: {
     type: Number,
-    default: 0,
+    default: 0
   },
   category: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Category',
-    required: true,
+    required: true
   },
   countInStock: {
     type: Number,
-    required: true,
+    required: true
   },
   rating: {
     type: Number,
-    default: 0,
+    default: 0
   },
   numReviews: {
     type: Number,
-    default: 0,
+    default: 0
   },
   isFeatured: {
     type: Boolean,
-    default: false,
+    default: false
   },
   dateCreated: {
     type: Date,
-    default: Date.now,
+    default: Date.now
   },
 });
 
